bbc7587eaf304f4b87a2a2ea53b5253e9d154643
App Academy Student <clementpigeon@gmail.com>
2013-09-18 16:04:10 -0400

    adds sub editing and update, with validation

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -14,7 +14,8 @@ class SubsController < ApplicationController
   end
 
   def create
-    @sub = User.new(params[:sub])
+    user_id = User.find_by_user_name(params[:sub][:user_name]).id
+    @sub = Sub.new(name: params[:sub][:name], user_id: user_id)
 
     if @sub.save
       redirect_to sub_url(@sub)
@@ -25,12 +26,32 @@ class SubsController < ApplicationController
   end
 
   def edit
+    @sub = Sub.find(params[:id])
   end
 
   def update
+    # TODO kill it with fire...figure out more elegant way to handle bad input
+    if params[:sub][:name] == "" || params[:sub][:user_name] == ""
+      flash[:errors] = ["You're fillin out the form wrong, brah!"]
+      redirect_to edit_sub_url(Sub.find(params[:id]))
+    elsif User.find_by_user_name(params[:sub][:user_name]).nil?
+      flash[:errors] = ["That user doesn't exist!"]
+      redirect_to edit_sub_url(Sub.find(params[:id]))
+    else
+      user_id = User.find_by_user_name(params[:sub][:user_name]).id
+      @sub = Sub.find(user_id)
+      if @sub.update_attributes(name: params[:sub][:name], user_id: user_id)
+        redirect_to sub_url(@sub)
+      else
+        flash[:errors] = @sub.errors.full_messages
+        render :edit
+      end
+    end
   end
 
-  def delete
+  def destroy
+    Sub.delete(params[:id])
+    redirect_to subs_url
   end
 
 end
